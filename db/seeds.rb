296109d07ce87f3ca2b1155ba91e304ba661e7e5
James Jefferies <james@shedcode.co.uk>
2012-10-13 15:17:27 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,7 @@
 Station.create(:name => "Dore", :csr => "DOR", :lat=>-1.515238, :lng=>53.327561);
 Station.create(:name => "Grindleford", :csr => "GRN", :lat=>-1.625817, :lng=>53.305531);
 Station.create(:name => "Sheffield", :csr => "SHF", :lat=>-1.461857, :lng=>53.378055)
-
+Station.create(:name => "Hathersage", :csr => "HSG", :lat=>-1.651794, :lng=>53.325736)
+Station.create(:name => "Bamford", :csr => "BAM", :lat=>-1.688542, :lng=>53.338862)
+Station.create(:name => "Hope", :csr => "HOP", :lat=>-1.730051, :lng=>53.346208)
+Station.create(:name => "Edale", :csr => "EDL", :lat=>-1.817258, :lng=>53.364773)
