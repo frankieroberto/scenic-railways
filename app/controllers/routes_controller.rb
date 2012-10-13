e38570e72b895173ff85c6fa8ce87914a7c072ba
Jez Nicholson <jez.nicholson@gmail.com>
2012-10-13 14:39:13 +0100

    Routes and Stations

---
+++ app/controllers/routes_controller.rb
@@ -0,0 +1,13 @@
+class RoutesController < ApplicationController
+  def index
+  end
+  
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ScenicRailways::Application.routes.draw do
 
   resources :scenic_routes, :only => [:show, :index]
+  resources :routes
+  resources :stations
 
   root :to => 'homepage#show'
 
