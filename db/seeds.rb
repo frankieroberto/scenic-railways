6d56f92567b5d9764a14743a56e78e45436a343c
James Jefferies <james@shedcode.co.uk>
2012-10-13 14:36:38 +0100

    adding seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Station.show();
+Station.create(name => "Sheffield", csr => "SHF", lat=-1.461857, lng=53.378055);
+Station.create(name => "Dore", csr => "DOR", lat=-1.515238, lng=53.327561);
+Station.create(name => "Grindleford", csr => "GRN", lat=-1.625817, lng=53.305531);
