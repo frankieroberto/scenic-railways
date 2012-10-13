7257a669b4e15195e2b7ce61876e539cab61c19a
James Jefferies <james@shedcode.co.uk>
2012-10-13 14:52:37 +0100

    Add stations controller

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,13 +1,11 @@
 class StationsController < ApplicationController
+  respond_to :html, :json
   def new
+    @station = Station.find(params[:id])
   end
-
-  def create
-  end
-
-  def edit
-  end
-
   def index
+    @stations = Station.all
+    respond_with @station
   end
 end
+
